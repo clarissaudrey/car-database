31be77970d8513033fe3fdd849b6642157ce0f97
NicGagnon <nicolas.z.gagnon@gmail.com>
2018-06-18 17:06:20 -0700

    Update customer.php

--- versionMasa/customer.php
+++ versionMasa/customer.php
@@ -8,22 +8,27 @@
 	<title></title>
 </head>
 <body>
-	<h1>  Welcome Customer! </h1>
+	<h1> Welcome to Team 42! </h1>
 	
 	<hr>
 
 <br> 	</br>
 <b> Find top (Input) driven vechicles</b>
-<form action="topDriven(Extra).php" method="GET">
-	<input type="number" name="field" placeholder="Input"><br>
-	<input type="submit" name="submit">
+<form action="topDriven(Extra).php" method="POST">
+	<input type="number" name="field" placeholder="Input">
+	<br>
+	<button type="submit" name="submit"> Submit </button>
 </form>
 
 <br> 	</br>
-<b> Search vehicle by gas type</b>
-<form action="gasType(Extra).php" method="GET">
-	<input type="text" name="gasType" placeholder="Gas Type"><br>
-	<input type="submit" name="submit">
+<b> Search vehicle with gas type</b>
+<form action="gasType(Extra).php" name='add' method="POST">
+Options: <select name='field'>
+     <option value='Gas' >Gas</option>
+     <option value='Diesel'>Diesel</option>
+     <option value='Electric'>Electric</option>
+     </select>
+<input type='submit' name='submit'/>
 </form>
 
 </body>
